b50f66329b88ed19e88b66c030c0841350a265b8
Kong Yang <kmongy05@gmail.com>
2025-04-05 16:51:24 -0500

    got scrolling functionality fixed to remove class for all blurbs.

--- src/main.js
+++ src/main.js
@@ -1,32 +1,26 @@
 import scrollama from "scrollama";
 
 let body = document.querySelector("body");
-let scrollIntro = body.querySelector("#intro");
-let sticky = scrollIntro.querySelector(".sticky-container");
-let article = scrollIntro.querySelector("article");
+let scrolling = body.querySelector(".story-element");
+let sticky = scrolling.querySelector(".sticky-container");
+let article = scrolling.querySelector("article");
 let blurbs = article.querySelectorAll(".blurb");
-
-let story1 = body.querySelector(".story-1");
 
 const scroller  = scrollama();
 
-function handleStepEnter(response) {
-  let el = response.element;
-
-  blurbs.forEach(blurb => blurb.classList.remove("active"));
-  el.classList.add("active");
-
-  sticky.querySelector("p").innerText = `Step number ${el.dataset.step}`;
-  sticky.style.backgroundColor = el.dataset.color;
-}
-
-
 scroller
   .setup({
-    step: "#intro article .blurb",
+    step: ".story-element article .blurb",
     offset: 0.50,
-    debug: false
+    debug: true
   })
-  .onStepEnter(handleStepEnter)
+  .onStepEnter((response) => {
+    let el = response.element;
+    el.classList.add("active");
+  })
+  .onStepExit((response) => {
+    let el = response.element;
+    el.classList.remove("active");
+  })
 
 console.log("Hi. I work!")
