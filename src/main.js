2327a2b721a4e032e76a54ebbeb264a6d7c14479
Kong Yang <kmongy05@gmail.com>
2025-04-06 15:27:32 -0500

    Updated with more functionality.

--- src/main.js
+++ src/main.js
@@ -6,17 +6,28 @@ let sticky = scrolling.querySelector(".sticky-container");
 let article = scrolling.querySelector("article");
 let blurbs = article.querySelectorAll(".blurb");
 
+let image1 = "./assets/headline1.png";
+let image2 = "./assets/testing1.png";
+
+
 const scroller  = scrollama();
 
 scroller
   .setup({
     step: ".story-element article .blurb",
     offset: 0.50,
-    debug: true
+    progress: true,
+    debug: false
   })
   .onStepEnter((response) => {
     let el = response.element;
     el.classList.add("active");
+
+    if(el.parentElement.className == "story-1-stepper") {
+      let container = document.querySelector("#story-1-data-container");
+
+      container.style.backgroundColor = "purple";
+    }
   })
   .onStepExit((response) => {
     let el = response.element;
