0c420e9de70abb9fd26eb5193c12126e2b4919a9
Kong Yang <kmongy05@gmail.com>
2025-04-30 12:31:47 -0500

    nrc js to change svgs were applied. Removed unused css styling.

--- src/main.js
+++ src/main.js
@@ -14,18 +14,19 @@ scroller
     let el = response.element;
     el.classList.add("active");
 
-    console.log('HI. INSIDE')
+    if(el.className.includes("nrc-surprise")) {
+      let container = document.querySelector(".may-bubble-nrc");
+
+      container.src = new URL("./components/may-bubble-surprise.svg", import.meta.url);
+    }
 
     if(el.className.includes("lexicon-match")) {
       let container = document.querySelector(".may-bubble-nrc");
 
-      container.src = new URL("./components/leave-common-word.svg", import.meta.url);
-      console.log(container);
+      container.src = new URL("./components/may-bubble-match-word.svg", import.meta.url);
     }
   })
   .onStepExit((response) => {
     let el = response.element;
     el.classList.remove("active");
   })
-
-console.log("Hi. I work!")
